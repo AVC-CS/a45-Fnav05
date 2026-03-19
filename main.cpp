eceae9e6699ba9cf99ab304278d581f1496ce90d
Fnav05 <felvinnavarro10@gmail.com>
2026-03-18 20:48:31 -0700

    A45 passed

--- main.cpp
+++ main.cpp
@@ -12,6 +12,30 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find min, max, and med among num1, num2, num3
+    min = num1;
+    if (num2 < min) {
+        min = num2;
+    }
+    if (num3 < min) {
+        min = num3;
+    }
 
+    max = num1;
+    if (num2 > max) {
+        max = num2;
+    }
+    if (num3 > max) {
+        max = num3;
+    }
+
+    if (num1 != min && num1 != max) {
+        med = num1;
+    }
+    else if (num2 != min && num2 != max) {
+        med = num2;
+    }
+    else {
+        med = num3;
+    }
     cout << min << " " << med << " " << max << endl;
 }
